a924aaf6296cf2f29f3a76ff397b21661750bb1b
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-02-28 00:24:45 +0300

    commit 1

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+
+import { Product } from '../models/product';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:7285/api/Products/getall';
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+   
+  }
+}
